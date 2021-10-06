2bbc3575ba2a2eb640f3d81beb84d3d00839ad7c
Rushiraj11 <grushiraj95@gmail.com>
2021-10-06 18:29:23 +0530

    Added:Ability to Calculate EmployeeWage Per Month

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -4,8 +4,8 @@ const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
 const PART_TIME_HRS=4;
 const FULL_TIME_HRS=8;
-const EMP_WAGE_PER_HRS=20
-
+const EMP_WAGE_PER_HRS=20;
+const NUM_OF_WORKING_DAYS= 20;
 
 function getWorkingHours(empCheck){
 switch(empCheck) {
@@ -20,9 +20,13 @@ switch(empCheck) {
    }    
 }
 
+
 let empHrs=0;
+for (let day=0; day<NUM_OF_WORKING_DAYS; day++) {
 let empCheck=Math.floor(Math.random()*10)%3;
-empHrs = getWorkingHours(empCheck);
+empHrs += getWorkingHours(empCheck);
+}
+
 let empWage=empHrs* EMP_WAGE_PER_HRS;
-console.log("Employee Wage is :" + empWage);
+console.log("Total Hrs: "+ empHrs +"Employee Wage is :" + empWage);
 
