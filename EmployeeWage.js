a8bd64cdc7c3a56d8efcedad41642c6afa4bf248
Rushiraj11 <grushiraj95@gmail.com>
2021-10-06 17:55:36 +0530

    Added:Ability to Calculate Employee daily wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,8 +1,25 @@
 console.log("Welcome to EmployeePayroll");
-const IS_PRESENT=1;
+const IS_ABSENT=0;
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HRS=4;
+const FULL_TIME_HRS=8;
+const EMP_WAGE_PER_HRS=20
 
-let empCheck=Math.floor(Math.random()*10)%2;
-if (empCheck==IS_PRESENT)
-    console.log("Employee is Present");
-else
-console.log("Employee is Absent");
+let empHrs=0;
+let empCheck=Math.floor(Math.random()*10)%3;
+switch(empCheck) {
+    
+    case IS_PART_TIME:
+        empHrs=PART_TIME_HRS;
+        break;
+    case IS_FULL_TIME:
+        empHrs=FULL_TIME_HRS;
+        break;
+    default:
+    empHrs=0;
+}    
+
+ let empWage=empHrs* EMP_WAGE_PER_HRS;
+  console.log("Employee Wage is :" + empWage);
+
