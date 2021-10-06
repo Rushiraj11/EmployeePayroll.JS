4d881151ec487ffd984f43cb98d8dea3de4c5817
Rushiraj11 <grushiraj95@gmail.com>
2021-10-06 11:11:36 +0530

    Added: Ability to check Employee Present or Absent

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,1 +1,8 @@
 console.log("Welcome to EmployeePayroll");
+const IS_PRESENT=1;
+
+let empCheck=Math.floor(Math.random()*10)%2;
+if (empCheck==IS_PRESENT)
+    console.log("Employee is Present");
+else
+console.log("Employee is Absent");
